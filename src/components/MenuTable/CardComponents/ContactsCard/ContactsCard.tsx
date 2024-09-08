5ac69b064e31d4719d188b00e9372c035f4bc286
dmitrij <leedukas123asdf@gmail.com>
2024-09-08 23:53:41 +0300

    center contacts

--- src/components/MenuTable/CardComponents/ContactsCard/ContactsCard.tsx
+++ src/components/MenuTable/CardComponents/ContactsCard/ContactsCard.tsx
@@ -3,7 +3,7 @@ import { MenuCard } from "@/components/MenuCard/MenuCard";
 export const ContactsCard = () => {
   return (
     <MenuCard>
-      <div className="flex flex-col h-full w-full px-8 pt-10">
+      <div className="flex flex-col h-full w-full items-center justify-center">
         <h2 className="text-2xl">Contacts:</h2>
         <ul className="list-disc">
           <li className="underline">dmitrij.sidorkin@gmail.com</li>
