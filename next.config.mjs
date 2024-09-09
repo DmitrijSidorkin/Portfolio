91c448b81e6c1a678f4f569f502eed37ee8860c4
dmitrij <leedukas123asdf@gmail.com>
2024-09-09 19:41:12 +0300

    deployment settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+const nextConfig = {
+  output: "export",
+};
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+    };
+  }
+
+  // Production config
+  return {
+    swcMinify: true,
+    images: {
+      loader: "custom",
+      loaderFile: "./cfImageLoader.js",
+    },
+  };
+};
 
 export default nextConfig;
