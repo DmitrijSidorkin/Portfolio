4d7a1d030218db81fb0f8c3d3cf57650de3feff8
dmitrij <leedukas123asdf@gmail.com>
2024-09-09 19:47:48 +0300

    coment custom image loading out

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
+// import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
 const nextConfig = {
   output: "export",
 };
 
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-    };
-  }
+// module.exports = (phase, { defaultConfig }) => {
+//   if (phase === PHASE_DEVELOPMENT_SERVER) {
+//     return {
+//       reactStrictMode: true,
+//     };
+//   }
 
-  // Production config
-  return {
-    swcMinify: true,
-    images: {
-      loader: "custom",
-      loaderFile: "./cfImageLoader.js",
-    },
-  };
-};
+//   // Production config
+//   return {
+//     swcMinify: true,
+//     images: {
+//       loader: "custom",
+//       loaderFile: "./cfImageLoader.js",
+//     },
+//   };
+// };
 
 export default nextConfig;
