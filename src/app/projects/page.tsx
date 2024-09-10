0f8021d61fbe20eac95f4f2ce543ac6847c5c0f5
dmitrij <leedukas123asdf@gmail.com>
2024-09-10 17:23:21 +0300

    update pixelify link

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,8 +4,7 @@ const projectsData = [
   {
     title: "Pixelify",
     image: "/pixelify-preview.jpg",
-    gitlink:
-      "https://github.com/DmitrijSidorkin/pixel-thing?tab=readme-ov-file",
+    gitlink: "https://github.com/DmitrijSidorkin/Pixelify",
     description:
       "Pixelify is a simple app that uses RAWG.io API to fetch random video game data, and turns it into a quiz with a pixelated image of a video game and 4 guesses to chose from. It also has global highscore tracking for registered users and after the quiz session detailed information for every pixelated game can be viewed.",
   },
