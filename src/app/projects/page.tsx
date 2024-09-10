627841b7d16a5b87863af72e9861bc4589d3378f
dmitrij <leedukas123asdf@gmail.com>
2024-09-10 15:29:54 +0300

    add lunch app to projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,38 @@
 import { ProjectCard } from "@/components/ProjectCard/ProjectCard";
 
+const projectsData = [
+  {
+    title: "Pixelify",
+    image: "/pixelify-preview.jpg",
+    gitlink:
+      "https://github.com/DmitrijSidorkin/pixel-thing?tab=readme-ov-file",
+    description:
+      "Pixelify is a simple app that uses RAWG.io API to fetch random video game data, and turns it into a quiz with a pixelated image of a video game and 4 guesses to chose from. It also has global highscore tracking for registered users and after the quiz session detailed information for every pixelated game can be viewed.",
+  },
+  {
+    title: "Lunch App (Sourcery Academy Team Project)",
+    image: "/lunch-app-preview.jpg",
+    gitlink: "https://github.com/DmitrijSidorkin/Lunch-App-Sourcery-Academy-",
+    description:
+      "A private company lunch app that allows its workers to order food straight to the workplace, with free lunches every Friday! This is purely a front-end mock version of such app.",
+  },
+];
+
 export default function About() {
   return (
     <main className="flex flex-grow pt-10 justify-center items-start">
-      <div className="max-w-screen-lg">
-        <ProjectCard
-          title="Pixelify"
-          image="/pixelify-preview.jpg"
-          gitlink="https://github.com/DmitrijSidorkin/pixel-thing?tab=readme-ov-file"
-          description="Pixelify is a simple app that uses RAWG.io API to fetch random video game data, and turns it into a quiz with a pixelated image of a video game and 4 guesses to chose from. It also has global highscore tracking for registered users and after the quiz session detailed information for every pixelated game can be viewed."
-        />
+      <div className="max-w-screen-lg flex flex-col gap-5">
+        {projectsData.map((project, index) => {
+          return (
+            <ProjectCard
+              key={index}
+              title={project.title}
+              image={project.image}
+              gitlink={project.gitlink}
+              description={project.description}
+            />
+          );
+        })}
       </div>
     </main>
   );
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -23,11 +23,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
     >
       <MenuCard>
         <div className="flex lg:max-h-80 lg:flex-row flex-col items-center rounded-xl hover:bg-white hover:bg-opacity-15">
-          <img
-            src="/pixelify-preview.jpg"
-            alt="pixelify preview"
-            className="max-h-80 rounded-xl pl-0"
-          />
+          <img src={image} alt={title} className="max-h-80 rounded-xl pl-0" />
           <div className="p-8">
             <h2 className="text-5xl lg:pb-10 text-center pb-6">{title}</h2>
             <p className="text-xl">{description}</p>
